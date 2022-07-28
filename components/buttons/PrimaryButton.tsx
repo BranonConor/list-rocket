7d7adc90a041f47eb156ce247cce19c675c8df16
Branon Eusebio <branoneusebio@Branons-MacBook-Air.local>
2022-07-27 20:37:12 -0700

    Added typescript config and PrimaryButton component

---
+++ components/buttons/PrimaryButton.tsx
@@ -0,0 +1,58 @@
+import styled from 'styled-components';
+
+export enum BUTTON_VARIANTS {
+	'full' = '100%',
+	'large' = '130px',
+	'small' = '100px',
+}
+
+interface Props {
+	download: boolean;
+	onClick: () => void;
+	variant: 'full' | 'large' | 'small';
+	content: string;
+	icon: string;
+}
+
+export const PrimaryButton: React.FC<Props> = ({
+	onClick,
+	variant,
+	content,
+	icon,
+}: Props) => {
+	return (
+		<StyledButton onClick={onClick} variant={variant}>
+			{icon && <img src={icon} alt='' width='30' height='30' />}
+			{content}
+		</StyledButton>
+	);
+};
+
+interface StyleProps {
+	variant: string;
+}
+const StyledButton = styled.button<StyleProps>(
+	({ variant, theme: { colors } }) => `
+	width: ${BUTTON_VARIANTS[variant]};
+	minWidth: 100%;
+	height: 40px;
+	margin: 16px 0;
+	background: ${colors.button.defaultBg};
+	padding: 8px 16px;
+	box-sizing: border-box;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	border-radius: 5px;
+	border: none;
+	color: ${colors.white};
+	text-decoration: none;
+	outline: none;
+	transition: 0.10s ease all;
+	
+	&:hover {
+		cursor: pointer;
+		background: ${colors.button.hoverBg};
+	}
+`
+);
