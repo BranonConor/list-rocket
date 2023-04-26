a0fc71453f3e0a85dac621615cd88148b9a7e50b
Branon Eusebio <branoneusebio@Branons-MacBook-Pro.local>
2023-04-26 13:46:25 -0700

    Bruh this better work

--- contexts/EventContext.tsx
+++ contexts/EventContext.tsx
@@ -3,12 +3,13 @@ import { createContext, useContext, useEffect, useState } from 'react';
 import { UserContext } from './UserContext';
 import axios from 'axios';
 import { IEvent, IEventContext } from './types';
+import Pusher from 'pusher-js';
 
 export const EventContext = createContext<IEventContext | null>(null);
 
 export const EventProvider = (props) => {
 	const [events, setEvents] = useState<IEvent[]>([]);
-	const { user } = useContext(UserContext);
+	const { user, getUserData } = useContext(UserContext);
 
 	const getAllEvents = async () => {
 		const res = await axios.get(`/api/events`, {
@@ -22,6 +23,25 @@ export const EventProvider = (props) => {
 	useEffect(() => {
 		getAllEvents();
 	}, [user]);
+
+	//start a pusher channel for this user to listen to updates
+	useEffect(() => {
+		const pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_KEY, {
+			cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER,
+		});
+		//subscribe to the user channel
+		const channel = pusher.subscribe(`user-channel-${user?._id}`);
+		//bind a function to the user-channel-update trigger, update UI
+		channel.bind(`user-channel-update-${user?._id}`, (data) => {
+			//refresh the invites list for only the user in question if logged in
+			getUserData();
+			getAllEvents();
+		});
+		//unsubscribe to the user channel on cleanup
+		return () => {
+			pusher.disconnect();
+		};
+	}, [getUserData]);
 
 	return (
 		<EventContext.Provider value={{ events, getAllEvents }}>
--- contexts/UserContext.tsx
+++ contexts/UserContext.tsx
@@ -1,10 +1,8 @@
-import { createContext, useContext, useEffect, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 
 import axios from 'axios';
 import { useSession } from 'next-auth/react';
 import { IUser, IUserContext } from './types';
-import { EventContext } from './EventContext';
-import Pusher from 'pusher-js';
 
 export const UserContext = createContext<IUserContext | null>(null);
 
@@ -28,27 +26,6 @@ export const UserProvider = (props) => {
 	useEffect(() => {
 		getUserData();
 	}, [status]);
-
-	//start a pusher channel for this user to listen to updates
-	useEffect(() => {
-		const pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_KEY, {
-			cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER,
-		});
-		//subscribe to the user channel
-		const channel = pusher.subscribe(`user-channel-${user?._id}`);
-		//bind a function to the user-channel-update trigger, update UI
-		channel.bind(`user-channel-update-${user?._id}`, (data) => {
-			//refresh the invites list for only the user in question if logged in
-			if (data.user?._id === user?._id) {
-				getUserData();
-			}
-		});
-		//unsubscribe to the user channel on cleanup
-		return () => {
-			pusher.unsubscribe(`user-channel-${user?._id}`);
-			pusher.disconnect();
-		};
-	}, []);
 
 	return (
 		<UserContext.Provider value={{ user, getUserData }}>
