542e53b93db19a0369f21940d9111429abe45207
branonconor <branoneusebio@gmail.com>
2023-06-10 18:36:57 -0700

    Prepped the client for API call for removing collaborator

--- components/lists/CollaboratorsGrid.tsx
+++ components/lists/CollaboratorsGrid.tsx
@@ -5,15 +5,54 @@ import { ProfilePhoto } from '../ProfilePhoto';
 import { AddCollaborator } from '../events/AddCollaborator';
 import { WorkspaceContext } from '../../contexts/WorkspaceContext';
 import { Title } from '../typography/Title';
+import { Dialog } from '../Dialog';
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
 export const CollaboratorsGrid = () => {
-	const { currentEvent } = useContext(WorkspaceContext);
+	const { currentEvent, clearWorkspace, prepWorkspace } =
+		useContext(WorkspaceContext);
 	const [isAddCollaboratorButtonClicked, setIsAddCollaboratorButtonClicked] =
 		useState(false);
 	const [
 		editCollaboratorsButtonIsClicked,
 		setEditCollaboratorsButtonIsClicked,
 	] = useState(false);
+	const [deleteCollaboratorDialogIsOpen, setDeleteCollaboratorDialogIsOpen] =
+		useState(false);
+
+	const [userToDelete, setUserToDelete] = useState<{
+		id: string;
+		name: string;
+	} | null>(null);
+
+	//Removing a collaborator from an event
+	const handleDelete = async (e: any, eventId: string, userId: string) => {
+		e?.preventDefault();
+		try {
+			await axios.put(`/api/events/${eventId}`, {
+				data: {
+					eventId: eventId,
+					userId: userId,
+					action: 'remove-collaborator',
+				},
+			});
+			setEditCollaboratorsButtonIsClicked(false);
+			setDeleteCollaboratorDialogIsOpen(false);
+
+			//TODO - make it a pusher event
+			prepWorkspace(eventId);
+
+			toast.success('Collaborator removed 👋', {
+				toastId: 'remove-collaborator-toast',
+			});
+		} catch (error) {
+			console.log(error);
+			toast.error('Something went wrong, sorry! 😵‍💫', {
+				toastId: 'error-delete-event-toast',
+			});
+		}
+	};
 
 	return (
 		<StyledGrid
@@ -34,19 +73,31 @@ export const CollaboratorsGrid = () => {
 				<StyledH2 variant='heading3'>Collaborators:</StyledH2>
 				{currentEvent.collaborators?.map((collaborator) => {
 					return (
-						<StyledButton
+						<StyledAvatar
 							key={collaborator._id}
 							isInEditMode={editCollaboratorsButtonIsClicked}>
 							<ProfilePhoto
 								photo={collaborator.image}
 								dimensions='40px'
 							/>
-						</StyledButton>
+							{editCollaboratorsButtonIsClicked && (
+								<StyledDeleteCollaboratorButton
+									onClick={() => {
+										setDeleteCollaboratorDialogIsOpen(true);
+										setUserToDelete({
+											id: collaborator._id,
+											name: collaborator.name,
+										});
+									}}>
+									<img src='/icons/trash-red.svg' alt='' />
+								</StyledDeleteCollaboratorButton>
+							)}
+						</StyledAvatar>
 					);
 				})}
 				{currentEvent.pendingCollaborators?.map((collaborator) => {
 					return (
-						<StyledPendingButton
+						<StyledPendingAvatar
 							key={collaborator._id}
 							isInEditMode={editCollaboratorsButtonIsClicked}>
 							<ProfilePhoto
@@ -54,13 +105,26 @@ export const CollaboratorsGrid = () => {
 								dimensions='40px'
 							/>
 							<StyledPendingDot />
-						</StyledPendingButton>
+							{editCollaboratorsButtonIsClicked && (
+								<StyledDeleteCollaboratorButton
+									onClick={() => {
+										setDeleteCollaboratorDialogIsOpen(true);
+										setUserToDelete({
+											id: collaborator._id,
+											name: collaborator.name,
+										});
+									}}>
+									<img src='/icons/trash-red.svg' alt='' />
+								</StyledDeleteCollaboratorButton>
+							)}
+						</StyledPendingAvatar>
 					);
 				})}
 				{!isAddCollaboratorButtonClicked && (
 					<>
 						{editCollaboratorsButtonIsClicked ? (
 							<StyledCollaboratorControlsButton
+								isInEditMode={editCollaboratorsButtonIsClicked}
 								onClick={() =>
 									setEditCollaboratorsButtonIsClicked(
 										!editCollaboratorsButtonIsClicked
@@ -76,6 +140,9 @@ export const CollaboratorsGrid = () => {
 						) : (
 							<>
 								<StyledCollaboratorControlsButton
+									isInEditMode={
+										editCollaboratorsButtonIsClicked
+									}
 									onClick={() => {
 										setIsAddCollaboratorButtonClicked(true);
 										setEditCollaboratorsButtonIsClicked(
@@ -89,11 +156,15 @@ export const CollaboratorsGrid = () => {
 									/>
 								</StyledCollaboratorControlsButton>
 								<StyledCollaboratorControlsButton
-									onClick={() =>
+									isInEditMode={
+										editCollaboratorsButtonIsClicked
+									}
+									onClick={() => {
 										setEditCollaboratorsButtonIsClicked(
 											!editCollaboratorsButtonIsClicked
-										)
-									}>
+										);
+										setUserToDelete(null);
+									}}>
 									<img
 										src='/icons/pencil.svg'
 										alt='Add Collaborator'
@@ -113,6 +184,18 @@ export const CollaboratorsGrid = () => {
 					setIsAddCollaboratorButtonClicked={
 						setIsAddCollaboratorButtonClicked
 					}
+				/>
+			)}
+			{deleteCollaboratorDialogIsOpen && (
+				<Dialog
+					title='Delete Event'
+					description={`Are you sure you want to remove ${userToDelete?.name} from the event? All their list data will be lost.`}
+					cta={(e: any) =>
+						handleDelete(e, currentEvent?._id, userToDelete?.id)
+					}
+					buttonText='Delete'
+					setDialogIsOpen={setDeleteCollaboratorDialogIsOpen}
+					showCancelButton
 				/>
 			)}
 		</StyledGrid>
@@ -143,40 +226,43 @@ const StyledH2 = styled(Title)`
 	}
 `;
 
-interface IStyledButtonProps {
+interface IStyledAvatarProps {
 	isInEditMode: boolean;
 }
-const StyledButton = styled.button<IStyledButtonProps>(
-	({ isInEditMode }) => `
-	outline: none;
-	border: none;
-	background: none;
-	transition: 0.15s ease all;
-	padding: 0 4px;
-	cursor: pointer;
-
-	&:hover {
-		transform: scale(1.1);
-	}
-
-	img {
-		filter: ${isInEditMode ? 'grayscale(100%)' : 'none'};
-		transform: ${isInEditMode ? 'scale(1.2)' : 'none'};
-	}
-`
-);
-const StyledPendingButton = styled.button<IStyledButtonProps>(
+const StyledAvatar = styled.div<IStyledAvatarProps>(
 	({ isInEditMode }) => `
 	outline: none;
 	border: none;
 	background: none;
 	transition: 0.15s ease all;
 	padding:  ${isInEditMode ? '0 8px' : '0 4px'};
+	box-sizing: border-box;
+	position: relative;
+	cursor: pointer;
+
+	&:hover {
+		transform: scale(1.1);
+	}
+
+	img {
+		filter: ${isInEditMode ? 'grayscale(100%)' : 'none'};
+		transform: ${isInEditMode ? 'scale(1.2)' : 'none'};
+	}
+`
+);
+const StyledPendingAvatar = styled.div<IStyledAvatarProps>(
+	({ isInEditMode }) => `
+	outline: none;
+	border: none;
+	background: none;
+	transition: 0.15s ease all;
+	box-sizing: border-box;
+	padding:  ${isInEditMode ? '0 8px' : '0 4px'};
 	position: relative;
 	cursor: pointer;
 
 	img {
-		opacity: 0.4;
+		opacity: ${isInEditMode ? '1' : '0.4'};
 	}
 
 	&:hover {
@@ -189,7 +275,8 @@ const StyledPendingButton = styled.button<IStyledButtonProps>(
 	}
 `
 );
-const StyledCollaboratorControlsButton = styled.button`
+const StyledCollaboratorControlsButton = styled.button<IStyledAvatarProps>(
+	({ isInEditMode }) => `
 	margin: 0;
 	display: flex;
 	align-items: center;
@@ -197,7 +284,7 @@ const StyledCollaboratorControlsButton = styled.button`
 	border: none;
 	background: none;
 	transition: 0.15s ease all;
-	filter: grayscale(100%);
+	filter: ${isInEditMode ? 'grayscale(0%)' : 'grayscale(100%)'};
 	padding: 0 0 0 12px;
 	transform: translateY(-2px);
 
@@ -210,7 +297,8 @@ const StyledCollaboratorControlsButton = styled.button`
 		width: 16px;
 		height: 16px;
 	}
-`;
+`
+);
 const StyledPendingDot = styled.div`
 	position: absolute;
 	bottom: 2px;
@@ -220,4 +308,25 @@ const StyledPendingDot = styled.div`
 	background: #decd32;
 	border-radius: 100%;
 	border: 3px solid white;
+`;
+const StyledDeleteCollaboratorButton = styled.button`
+	border: none;
+	outline: none;
+	background: none;
+	position: absolute;
+	top: 6px;
+	left: 10px;
+	z-index: 10;
+	width: 14px;
+	height: 14px;
+	cursor: pointer;
+
+	img {
+		width: 16px;
+		height: 16px;
+		filter: grayscale(0%);
+		background: rgba(250, 250, 250, 0.85);
+		border-radius: 100%;
+		padding: 4px;
+	}
 `;
