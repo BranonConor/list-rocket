3ecaa4e722797addc36bda59188d7ff74357edd3
Branon Eusebio <branoneusebio@Branons-MacBook-Pro.local>
2023-10-07 14:37:54 -0700

    Moving image meta tag to layouts

--- components/layouts/LoadingLayout.tsx
+++ components/layouts/LoadingLayout.tsx
@@ -14,6 +14,10 @@ export const LoadingLayout = ({ children }) => {
 				/>
 				<meta
 					property='og:image'
+					content='https://www.listrocket.app/graphics/list-items.svg'
+				/>
+				<meta
+					property='og:image'
 					content={`https://og-image.vercel.app/${encodeURI(
 						siteTitle
 					)}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
