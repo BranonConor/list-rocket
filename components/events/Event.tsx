8a4a54169654b7350959580e3bbd49cc5caf26ac
Branon Eusebio <branoneusebio@gmail.com>
2023-12-28 14:59:48 -0800

    Abstracted out add block modal into separate component

--- components/events/Event.tsx
+++ components/events/Event.tsx
@@ -12,6 +12,7 @@ import { toast } from 'react-toastify';
 import axios from 'axios';
 import { UserContext } from '../../contexts/UserContext';
 import { SkeletonLoader } from '../layouts/SkeletonLoader';
+import { AddBlockModal } from './AddBlockModal';
 
 interface IEventProps {
 	currentEvent: any;
@@ -219,41 +220,10 @@ export const Event: React.FC<IEventProps> = ({ currentEvent }) => {
 				</>
 			)}
 			{blockModalIsOpen && (
-				<Dialog
-					maxWidth='50%'
-					title='✨ Add Event Blocks'
-					description={
-						'Customize your event by adding the blocks you need!'
-					}
-					buttonText={'Cancel'}
-					setDialogIsOpen={setBlockModalIsOpen}>
-					<StyledBlockSelection>
-						<StyledCard onClick={handleAddListBlock}>
-							<StyledTitle variant='heading3'>
-								<StyledIcon src='/icons/list.svg' />
-								List
-							</StyledTitle>
-							<Text variant='body2'>
-								Lists can be assigned a user and be filled with
-								various types of nifty List Items
-							</Text>
-						</StyledCard>
-						<StyledComingSoonCard>
-							<StyledTitle variant='heading3'>Poll</StyledTitle>
-							<Text variant='body2'>Coming soon! 👀</Text>
-						</StyledComingSoonCard>
-						<StyledComingSoonCard>
-							<StyledTitle variant='heading3'>Chats</StyledTitle>
-							<Text variant='body2'>Coming soon! 👀</Text>
-						</StyledComingSoonCard>
-						<StyledComingSoonCard>
-							<StyledTitle variant='heading3'>
-								Announcements
-							</StyledTitle>
-							<Text variant='body2'>Coming soon! 👀</Text>
-						</StyledComingSoonCard>
-					</StyledBlockSelection>
-				</Dialog>
+				<AddBlockModal
+					setBlockModalIsOpen={setBlockModalIsOpen}
+					handleAddListBlock={handleAddListBlock}
+				/>
 			)}
 			<StyledButtonWrapper>
 				<AddBlockButton onClick={() => setBlockModalIsOpen(true)} />
@@ -371,73 +341,6 @@ const StyledButtonWrapper = styled.div`
 		bottom: 70px;
 		right: 16px;
 	}
-`;
-const StyledBlockSelection = styled.div(
-	({ theme: { colors } }) => `
-	width: 100%;
-	display: grid;
-	grid-template-columns: 1fr 1fr;
-	grid-template-rows: 1fr 1fr;
-	grid-gap: 16px;
-	color: ${colors.font.body2};
-	padding: 32px 0;
-	
-	@media only screen and (max-width: 768px) {
-		grid-template-columns: 1fr;
-		max-height: 300px;
-		overflow: auto;
-		padding: 8px 0;
-	}
-`
-);
-const StyledCard = styled.div(
-	({ theme: { colors, shadows } }) => `
-	width: 100%;
-	background: ${colors.bgLight};
-	color: ${colors.font.body};
-	border-radius: 8px;
-	padding: 16px;
-	box-sizing: border-box;
-	transition: 0.15s ease all;
-
-	&:hover {
-		box-shadow: ${shadows.standard};
-		background: white;
-		transform: translateY(-3px);
-		cursor: pointer;
-	}
-
-	@media only screen and (max-width: 768px) {
-		padding: 8px 16px;
-	}
-`
-);
-const StyledComingSoonCard = styled.div(
-	({ theme: { colors } }) => `
-	width: 100%;
-	background: ${colors.bgLight};
-	color: ${colors.font.body};
-	border-radius: 8px;
-	padding: 16px;
-	box-sizing: border-box;
-	transition: 0.15s ease all;
-	opacity: 0.5;
-
-	&:hover {
-		cursor: not-allowed;
-	}
-
-	@media only screen and (max-width: 768px) {
-		padding: 8px 16px;
-	}
-`
-);
-const StyledIcon = styled.img`
-	margin: 0 8px 0 0;
-`;
-const StyledTitle = styled(Title)`
-	display: flex;
-	align-items: center;
 `;
 const StyledFlexWrapper = styled.div`
 	display: flex;
